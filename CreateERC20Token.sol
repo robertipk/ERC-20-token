791f8e82c25d25f59594ec09886cec73da14fb2b
Rob <robertipk@gmail.com>
2022-02-12 17:51:20 -0800

    Update CreateERC20Token.sol
    fix quotes

--- CreateERC20Token.sol
+++ CreateERC20Token.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Bob is ERC20 {
-    constructor(uint256 initialSupply) public ERC20(“Bob”, “BB”) {
+    constructor(uint256 initialSupply) public ERC20("Bob", "BB") {
         _mint(msg.sender, initialSupply);
     }
 }
